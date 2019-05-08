fac2ae2986ddbb22d103e1673dc9867877681d48
Lorenzon99 <matteolorenz99@gmail.com>
2019-05-08 12:37:39 +0200

    Lorenzon: implementata bottom bar    * cambio activity toccando icone    * cambio colore sfondo icona selezionata

---
+++ NIUKO/app/src/main/java/com/michelezulian/example/niuko/UserFragment.java
@@ -0,0 +1,15 @@
+package com.michelezulian.example.niuko;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View vView = inflater.inflate(R.layout.fragment_user, container, false);
+        return vView;
+    }
+}
