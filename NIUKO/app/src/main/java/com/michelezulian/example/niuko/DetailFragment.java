c9aebe3789b1019ecb240ec9ba352215f2964d0f
Lorenzon99 <matteolorenz99@gmail.com>
2019-05-22 17:26:16 +0200

    Migliorata grafica lista corsi Ora mostra immagini

---
+++ NIUKO/app/src/main/java/com/michelezulian/example/niuko/DetailFragment.java
@@ -0,0 +1,11 @@
+package com.michelezulian.example.niuko;
+
+import android.app.Fragment;
+
+public class DetailFragment extends Fragment {
+    int mId;
+
+    public DetailFragment() {
+
+    }
+}
--- NIUKO/app/src/main/java/com/michelezulian/example/niuko/ExploreFragment.java
+++ NIUKO/app/src/main/java/com/michelezulian/example/niuko/ExploreFragment.java
@@ -1,10 +1,13 @@
 package com.michelezulian.example.niuko;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -19,12 +22,23 @@ public class ExploreFragment extends Fragment {
 
         ArrayList<Corso> vCorsi = new ArrayList<>();
 
-        vCorsi.add(new Corso(1,"Inglese Avanzato", "Padova", "https://www.mlaworld.com/blog/wp-content/uploads/2016/07/parole-slang-inglese.jpg", 32));
-        vCorsi.add(new Corso(2,"Francese Arretrato", "Padova", "https://www.neweuropelingue.it/wp-content/uploads/2018/08/corsi-francese-vomero.jpg", 32));
+        vCorsi.add(new Corso(1,"Inglese Avanzato", "Padova","http://www.metup.it/wp-content/uploads/2016/06/Google-Immagini-Homepage-1.jpg" , 32));
+        vCorsi.add(new Corso(2,"Francese Arretrato", "Padova", "https://www.neweuropelingue.it/wp-content/uploads/2018/08/corsi-francese-vomero.jpg" , 32));
         vCorsi.add(new Corso(3,"Informatica Avanzata Nel Campo Delle Nanotecnologie E Pasta Alla Carbonara", "Padova", "https://www.informacibo.it/wp-content/uploads/2018/04/carbonara.jpg", 32));
 
         CorsoAdapter vAdapter = new CorsoAdapter(vCorsi, getActivity());
         mListView.setAdapter(vAdapter);
+        /*
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FragmentManager vManager = getFragmentManager();
+                FragmentTransaction vTransaction = vManager.beginTransaction();
+                vTransaction.replace(R.id.fragment_container, new DetailFragment());
+                vTransaction.commit();
+            }
+        });
+        */
 
         return vView;
     }
